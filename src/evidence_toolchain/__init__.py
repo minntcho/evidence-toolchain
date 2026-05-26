ca110666a39637f856ae429e6dad36bb11275414
minntcho <limsh12069@gmail.com>
2026-05-26 18:45:27 +0900

    write evidence report artifacts

--- src/evidence_toolchain/__init__.py
+++ src/evidence_toolchain/__init__.py
@@ -9,7 +9,11 @@ from evidence_toolchain.capabilities import (
 from evidence_toolchain.observations import EvidenceObservation, observe_document
 from evidence_toolchain.planner import EvidenceToolPlan, plan_document
 from evidence_toolchain.preflight import EvidencePreflight, preflight_document
-from evidence_toolchain.reports import EvidenceReport, emit_evidence_report
+from evidence_toolchain.reports import (
+    EvidenceReport,
+    emit_evidence_report,
+    write_evidence_report,
+)
 from evidence_toolchain.routers import ObservationRouter, RuleObservationRouter
 from evidence_toolchain.runners import run_document
 from evidence_toolchain.runtime import (
@@ -38,5 +42,6 @@ __all__ = [
     "plan_document",
     "preflight_document",
     "emit_evidence_report",
+    "write_evidence_report",
     "run_document",
 ]
